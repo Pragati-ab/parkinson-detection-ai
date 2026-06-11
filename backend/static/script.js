1c0c9fc04b217ca2f53ea218e279f987b5dec32f
Pragati <12khushi11sharma@gmail.com>
2026-06-11 16:10:28 +0530

    render issue

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,197 +1,111 @@
 document
-    .getElementById("predictionForm")
-    .addEventListener("submit", async function (e) {
+  .getElementById("predictionForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-        e.preventDefault();
+    // Collect Input Values
+    const data = {
+      spread1: parseFloat(document.getElementById("spread1").value),
 
-        // Collect Input Values
-        const data = {
-            spread1: parseFloat(
-                document.getElementById("spread1").value
-            ),
+      PPE: parseFloat(document.getElementById("PPE").value),
 
-            PPE: parseFloat(
-                document.getElementById("PPE").value
-            ),
+      spread2: parseFloat(document.getElementById("spread2").value),
 
-            spread2: parseFloat(
-                document.getElementById("spread2").value
-            ),
+      HNR: parseFloat(document.getElementById("HNR").value),
 
-            HNR: parseFloat(
-                document.getElementById("HNR").value
-            ),
+      MDVP_Shimmer: parseFloat(document.getElementById("MDVP_Shimmer").value),
 
-            MDVP_Shimmer: parseFloat(
-                document.getElementById("MDVP_Shimmer").value
-            ),
+      voice_amplitude_instability: parseFloat(
+        document.getElementById("voice_amplitude_instability").value,
+      ),
 
-            voice_amplitude_instability: parseFloat(
-                document.getElementById(
-                    "voice_amplitude_instability"
-                ).value
-            ),
+      MDVP_APQ: parseFloat(document.getElementById("MDVP_APQ").value),
 
-            MDVP_APQ: parseFloat(
-                document.getElementById("MDVP_APQ").value
-            ),
+      MDVP_Flo_Hz: parseFloat(document.getElementById("MDVP_Flo_Hz").value),
 
-            MDVP_Flo_Hz: parseFloat(
-                document.getElementById(
-                    "MDVP_Flo_Hz"
-                ).value
-            ),
+      Shimmer_APQ5: parseFloat(document.getElementById("Shimmer_APQ5").value),
 
-            Shimmer_APQ5: parseFloat(
-                document.getElementById(
-                    "Shimmer_APQ5"
-                ).value
-            ),
+      D2: parseFloat(document.getElementById("D2").value),
 
-            D2: parseFloat(
-                document.getElementById("D2").value
-            ),
+      MDVP_Shimmer_dB: parseFloat(
+        document.getElementById("MDVP_Shimmer_dB").value,
+      ),
 
-            MDVP_Shimmer_dB: parseFloat(
-                document.getElementById(
-                    "MDVP_Shimmer_dB"
-                ).value
-            ),
+      Shimmer_DDA: parseFloat(document.getElementById("Shimmer_DDA").value),
 
-            Shimmer_DDA: parseFloat(
-                document.getElementById(
-                    "Shimmer_DDA"
-                ).value
-            ),
+      Shimmer_APQ3: parseFloat(document.getElementById("Shimmer_APQ3").value),
 
-            Shimmer_APQ3: parseFloat(
-                document.getElementById(
-                    "Shimmer_APQ3"
-                ).value
-            ),
+      MDVP_Jitter_Abs: parseFloat(
+        document.getElementById("MDVP_Jitter_Abs").value,
+      ),
 
-            MDVP_Jitter_Abs: parseFloat(
-                document.getElementById(
-                    "MDVP_Jitter_Abs"
-                ).value
-            ),
+      MDVP_Fo_Hz: parseFloat(document.getElementById("MDVP_Fo_Hz").value),
+    };
 
-            MDVP_Fo_Hz: parseFloat(
-                document.getElementById(
-                    "MDVP_Fo_Hz"
-                ).value
-            )
-        };
+    try {
+      // API Call
+      const response = await fetch(
+        "https://parkinson-detection-ai.onrender.com/predict",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        },
+      );
 
-        try {
+      const result = await response.json();
 
-            // API Call
-            const response = await fetch(
-                "http://127.0.0.1:8001/predict",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type":
-                            "application/json"
-                    },
-                    body: JSON.stringify(data)
-                }
-            );
+      // Get Result Elements
+      const resultCard = document.getElementById("resultCard");
 
-            const result =
-                await response.json();
+      const resultTitle = document.getElementById("resultTitle");
 
-            // Get Result Elements
-            const resultCard =
-                document.getElementById(
-                    "resultCard"
-                );
+      const confidenceText = document.getElementById("confidenceText");
 
-            const resultTitle =
-                document.getElementById(
-                    "resultTitle"
-                );
+      const confidenceFill = document.getElementById("confidenceFill");
 
-            const confidenceText =
-                document.getElementById(
-                    "confidenceText"
-                );
+      const recommendationText = document.getElementById("recommendationText");
 
-            const confidenceFill =
-                document.getElementById(
-                    "confidenceFill"
-                );
+      // Show Result Card
+      resultCard.classList.remove("hidden");
 
-            const recommendationText =
-                document.getElementById(
-                    "recommendationText"
-                );
+      // Positive Result
+      if (result.prediction === 1) {
+        resultCard.classList.remove("result-negative");
 
-            // Show Result Card
-            resultCard.classList.remove(
-                "hidden"
-            );
+        resultCard.classList.add("result-positive");
 
-            // Positive Result
-            if (
-                result.prediction === 1
-            ) {
+        resultTitle.innerHTML = "⚠ Parkinson Risk Detected";
 
-                resultCard.classList.remove(
-                    "result-negative"
-                );
+        recommendationText.innerHTML =
+          "Please consult a neurologist for further medical evaluation.";
+      }
 
-                resultCard.classList.add(
-                    "result-positive"
-                );
+      // Healthy Result
+      else {
+        resultCard.classList.remove("result-positive");
 
-                resultTitle.innerHTML =
-                    "⚠ Parkinson Risk Detected";
+        resultCard.classList.add("result-negative");
 
-                recommendationText.innerHTML =
-                    "Please consult a neurologist for further medical evaluation.";
+        resultTitle.innerHTML = "✅ No Strong Parkinson Indicators";
 
-            }
+        recommendationText.innerHTML =
+          "No strong signs detected. Maintain regular health checkups.";
+      }
 
-            // Healthy Result
-            else {
+      confidenceText.innerHTML = `Confidence: ${result.confidence}%`;
 
-                resultCard.classList.remove(
-                    "result-positive"
-                );
+      confidenceFill.style.width = `${result.confidence}%`;
 
-                resultCard.classList.add(
-                    "result-negative"
-                );
+      // Scroll to result
+      resultCard.scrollIntoView({
+        behavior: "smooth",
+      });
+    } catch (error) {
+      alert("Error connecting to backend.");
 
-                resultTitle.innerHTML =
-                    "✅ No Strong Parkinson Indicators";
-
-                recommendationText.innerHTML =
-                    "No strong signs detected. Maintain regular health checkups.";
-
-            }
-
-            confidenceText.innerHTML =
-                `Confidence: ${result.confidence}%`;
-
-            confidenceFill.style.width =
-                `${result.confidence}%`;
-
-            // Scroll to result
-            resultCard.scrollIntoView({
-                behavior: "smooth"
-            });
-
-        }
-
-        catch (error) {
-
-            alert(
-                "Error connecting to backend."
-            );
-
-            console.error(error);
-        }
-
-    });
+      console.error(error);
+    }
+  });
